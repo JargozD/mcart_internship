76fe4f6e20033d343e01205f34856459b6f3e987
Yurii Alexeev <kurebek777@gmail.com>
2022-10-06 11:24:51 +0000

    Добавление компонентов в шаблон и главную страницу

--- index.php
+++ index.php
@@ -1,7 +1,69 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-$APPLICATION->SetTitle("Мебельная компания");
+$APPLICATION->SetPageProperty("title", "Биржа недвижимости");
+$APPLICATION->SetPageProperty("keywords", "дома, дачи, коттеджи, таунхаусы");
+$APPLICATION->SetPageProperty("description", "На данном сайте вы можете подобрать для себя недвижимость согласно вашим предпочтениям, а так-же разместить объявление и продать собственную недвижимость. Сделать это очень просто.");
+$APPLICATION->SetTitle("Главная страница");
 ?>
+
+
+<? $APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"",
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array("", ""),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "adverts",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array("", "Приоритетная", ""),
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N"
+	)
+); ?>
 
 <div class="slide-one-item home-slider owl-carousel">
 
@@ -40,29 +102,50 @@ $APPLICATION->SetTitle("Мебельная компания");
 		<div class="row">
 			<div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
 				<div class="feature d-flex align-items-start">
-					<span class="icon mr-3 flaticon-house"></span>
-					<div class="text">
-						<h2 class="mt-0">Wide Range of Properties</h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.</p>
-					</div>
+					<? $APPLICATION->IncludeComponent(
+						"bitrix:main.include",
+						".default",
+						array(
+							"AREA_FILE_SHOW" => "file",
+							"AREA_FILE_SUFFIX" => "inc",
+							"EDIT_TEMPLATE" => "",
+							"PATH" => "/include/index_wide_range.php",
+							"COMPONENT_TEMPLATE" => ".default"
+						),
+						false
+					); ?>
 				</div>
 			</div>
 			<div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
 				<div class="feature d-flex align-items-start">
-					<span class="icon mr-3 flaticon-rent"></span>
-					<div class="text">
-						<h2 class="mt-0">Rent or Sale</h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.</p>
-					</div>
+					<? $APPLICATION->IncludeComponent(
+						"bitrix:main.include",
+						".default",
+						array(
+							"AREA_FILE_SHOW" => "file",
+							"AREA_FILE_SUFFIX" => "inc",
+							"EDIT_TEMPLATE" => "",
+							"PATH" => "/include/index_rent_or_sale.php",
+							"COMPONENT_TEMPLATE" => ".default"
+						),
+						false
+					); ?>
 				</div>
 			</div>
 			<div class="col-md-6 col-lg-4 mb-3 mb-lg-0">
 				<div class="feature d-flex align-items-start">
-					<span class="icon mr-3 flaticon-location"></span>
-					<div class="text">
-						<h2 class="mt-0">Property Location</h2>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit rem sint debitis porro quae dolorum neque.</p>
-					</div>
+					<? $APPLICATION->IncludeComponent(
+						"bitrix:main.include",
+						".default",
+						array(
+							"AREA_FILE_SHOW" => "file",
+							"AREA_FILE_SUFFIX" => "inc",
+							"EDIT_TEMPLATE" => "",
+							"PATH" => "/include/index_property_location.php",
+							"COMPONENT_TEMPLATE" => ".default"
+						),
+						false
+					); ?>
 				</div>
 			</div>
 		</div>
@@ -73,6 +156,27 @@ $APPLICATION->SetTitle("Мебельная компания");
 		<div class="row mb-5">
 			<div class="col-12">
 				<div class="site-section-title">
+
+					<? $APPLICATION->IncludeComponent(
+						"bitrix:news.line",
+						"",
+						array(
+							"ACTIVE_DATE_FORMAT" => "d.m.Y",
+							"CACHE_GROUPS" => "Y",
+							"CACHE_TIME" => "300",
+							"CACHE_TYPE" => "A",
+							"DETAIL_URL" => "",
+							"FIELD_CODE" => array("", ""),
+							"IBLOCKS" => array(),
+							"IBLOCK_TYPE" => "adverts",
+							"NEWS_COUNT" => "9",
+							"SORT_BY1" => "ACTIVE_FROM",
+							"SORT_BY2" => "SORT",
+							"SORT_ORDER1" => "DESC",
+							"SORT_ORDER2" => "ASC"
+						)
+					); ?>
+
 					<h2>New Properties for You</h2>
 				</div>
 			</div>
@@ -295,6 +399,34 @@ $APPLICATION->SetTitle("Мебельная компания");
 			<div class="col-md-7 text-center mb-5">
 				<div class="site-section-title">
 					<h2>Our Services</h2>
+
+					<? $APPLICATION->IncludeComponent(
+	"bitrix:news.line", 
+	".default", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "300",
+		"CACHE_TYPE" => "A",
+		"DETAIL_URL" => "",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"IBLOCKS" => array(
+			0 => "6",
+		),
+		"IBLOCK_TYPE" => "services",
+		"NEWS_COUNT" => "3",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+); ?>
+
 				</div>
 			</div>
 		</div>
@@ -353,6 +485,32 @@ $APPLICATION->SetTitle("Мебельная компания");
 			<div class="col-md-7 text-center">
 				<div class="site-section-title">
 					<h2>Our Blog</h2>
+
+					<? $APPLICATION->IncludeComponent(
+						"bitrix:news.line",
+						".default",
+						array(
+							"ACTIVE_DATE_FORMAT" => "d.m.Y",
+							"CACHE_GROUPS" => "Y",
+							"CACHE_TIME" => "300",
+							"CACHE_TYPE" => "A",
+							"DETAIL_URL" => "",
+							"FIELD_CODE" => array(
+								0 => "",
+								1 => "",
+							),
+							"IBLOCKS" => array(),
+							"IBLOCK_TYPE" => "news",
+							"NEWS_COUNT" => "3",
+							"SORT_BY1" => "ACTIVE_FROM",
+							"SORT_BY2" => "SORT",
+							"SORT_ORDER1" => "DESC",
+							"SORT_ORDER2" => "ASC",
+							"COMPONENT_TEMPLATE" => ".default"
+						),
+						false
+					); ?>
+
 				</div>
 			</div>
 		</div>
@@ -394,6 +552,34 @@ $APPLICATION->SetTitle("Мебельная компания");
 			<div class="col-md-7">
 				<div class="site-section-title text-center">
 					<h2>Our Agents</h2>
+
+					<? $APPLICATION->IncludeComponent(
+	"bitrix:news.line", 
+	".default", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "300",
+		"CACHE_TYPE" => "A",
+		"DETAIL_URL" => "",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"IBLOCKS" => array(
+			0 => "7",
+		),
+		"IBLOCK_TYPE" => "agents",
+		"NEWS_COUNT" => "3",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+); ?>
+
 				</div>
 			</div>
 		</div>
--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -2,115 +2,201 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 ?>
 
-<?php
-IncludeTemplateLangFile(__FILE__);
-?>
+    <?php
+    IncludeTemplateLangFile(__FILE__);
+    ?>
 
 
-<!DOCTYPE html>
-<html lang="<?= LANGUAGE_ID ?>">
+    <!DOCTYPE html>
+    <html lang="<?= LANGUAGE_ID ?>">
 
-<head>
-  <?
-  $APPLICATION->ShowHead();
+    <head>
+      <?
+      $APPLICATION->ShowHead();
 
-  $APPLICATION->SetAdditionalCSS("https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/fonts/icomoon/style.css");
+      $APPLICATION->SetAdditionalCSS("https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/fonts/icomoon/style.css");
 
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/bootstrap.min.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/magnific-popup.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/jquery-ui.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/owl.carousel.min.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/owl.theme.default.min.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/bootstrap-datepicker.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/mediaelementplayer.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/animate.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/fonts/flaticon/font/flaticon.css");
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/fl-bigmug-line.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/bootstrap.min.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/magnific-popup.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/jquery-ui.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/owl.carousel.min.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/owl.theme.default.min.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/bootstrap-datepicker.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/mediaelementplayer.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/animate.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/fonts/flaticon/font/flaticon.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/fl-bigmug-line.css");
 
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/aos.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/aos.css");
 
-  $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/style.css");
+      $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . "/css/style.css");
 
-  ?>
-  <title><? $APPLICATION->ShowTitle(); ?></title>
-</head>
 
-<body>
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/jquery-3.3.1.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/jquery-migrate-3.0.1.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/jquery-ui.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/popper.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/bootstrap.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/owl.carousel.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/mediaelement-and-player.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/jquery.stellar.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/jquery.countdown.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/jquery.magnific-popup.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/bootstrap-datepicker.min.js');
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/aos.js');
 
-  <div id="panel"><? $APPLICATION->ShowPanel() ?></div>
+      $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/main.js');
 
-  <div class="site-loader"></div>
+      ?>
+      <title><? $APPLICATION->ShowTitle(); ?></title>
+    </head>
 
-  <div class="site-wrap">
+    <body>
 
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
+      <div id="panel"><? $APPLICATION->ShowPanel() ?></div>
+
+      <div class="site-loader"></div>
+
+      <div class="site-wrap">
+
+        <div class="site-mobile-menu">
+          <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+              <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+          </div>
+          <div class="site-mobile-menu-body"></div>
+        </div> <!-- .site-mobile-menu -->
+
+        <div class="border-bottom bg-white top-bar">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-6 col-md-6">
+                <p class="mb-0">
+                  <a href="#" class="mr-3">
+                    <? $APPLICATION->IncludeComponent(
+                      "bitrix:main.include",
+                      ".default",
+                      array(
+                        "AREA_FILE_SHOW" => "file",
+                        "AREA_FILE_SUFFIX" => "inc",
+                        "EDIT_TEMPLATE" => "",
+                        "PATH" => "/include/header_phone.php",
+                        "COMPONENT_TEMPLATE" => ".default"
+                      ),
+                      false
+                    ); ?>
+                  </a>
+                  <a href="#">
+                    <? $APPLICATION->IncludeComponent(
+                      "bitrix:main.include",
+                      ".default",
+                      array(
+                        "AREA_FILE_SHOW" => "file",
+                        "AREA_FILE_SUFFIX" => "inc",
+                        "EDIT_TEMPLATE" => "",
+                        "PATH" => "/include/header_mail.php",
+                        "COMPONENT_TEMPLATE" => ".default"
+                      ),
+                      false
+                    ); ?>
+                  </a>
+                </p>
+              </div>
+              <div class="col-6 col-md-6 text-right">
+                <? $APPLICATION->IncludeComponent(
+                  "bitrix:main.include",
+                  ".default",
+                  array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/header_icons.php",
+                    "COMPONENT_TEMPLATE" => ".default"
+                  ),
+                  false
+                ); ?>
+              </div>
+            </div>
+          </div>
+
         </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div> <!-- .site-mobile-menu -->
+        <div class="site-navbar">
+          <div class="container py-1">
+            <div class="row align-items-center">
+              <div class="col-8 col-md-8 col-lg-4">
+                <h1 class="">
+                  <? $APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    ".default",
+                    array(
+                      "AREA_FILE_SHOW" => "file",
+                      "AREA_FILE_SUFFIX" => "inc",
+                      "EDIT_TEMPLATE" => "",
+                      "PATH" => "/include/header_logo.php",
+                      "COMPONENT_TEMPLATE" => ".default"
+                    ),
+                    false
+                  ); ?>
+                </h1>
+              </div>
+              <div class="col-4 col-md-4 col-lg-8">
+                <nav class="site-navigation text-right text-md-right" role="navigation">
 
-    <div class="border-bottom bg-white top-bar">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-6 col-md-6">
-            <p class="mb-0">
-              <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
-              <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
-            </p>
-          </div>
-          <div class="col-6 col-md-6 text-right">
-            <a href="#" class="mr-3"><span class="text-black icon-facebook"></span></a>
-            <a href="#" class="mr-3"><span class="text-black icon-twitter"></span></a>
-            <a href="#" class="mr-0"><span class="text-black icon-linkedin"></span></a>
-          </div>
-        </div>
-      </div>
+                  <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
 
-    </div>
-    <div class="site-navbar">
-      <div class="container py-1">
-        <div class="row align-items-center">
-          <div class="col-8 col-md-8 col-lg-4">
-            <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span class="text-danger">.</span></strong></a></h1>
-          </div>
-          <div class="col-4 col-md-4 col-lg-8">
-            <nav class="site-navigation text-right text-md-right" role="navigation">
-
-              <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-              <ul class="site-menu js-clone-nav d-none d-lg-block">
-                <li class="active">
-                  <a href="index.html">Home</a>
-                </li>
-                <li class="has-children">
-                  <a href="properties.html">Properties</a>
-                  <ul class="dropdown">
-                    <li><a href="#">Buy</a></li>
-                    <li><a href="#">Rent</a></li>
-                    <li><a href="#">Lease</a></li>
+                  <ul class="site-menu js-clone-nav d-none d-lg-block">
+                    <li class="active">
+                      <a href="index.html">Home</a>
+                    </li>
                     <li class="has-children">
-                      <a href="#">Menu</a>
+                      <a href="properties.html">Properties</a>
                       <ul class="dropdown">
-                        <li><a href="#">Menu One</a></li>
-                        <li><a href="#">Menu Two</a></li>
-                        <li><a href="#">Menu Three</a></li>
+                        <li><a href="#">Buy</a></li>
+                        <li><a href="#">Rent</a></li>
+                        <li><a href="#">Lease</a></li>
+                        <li class="has-children">
+                          <a href="#">Menu</a>
+                          <ul class="dropdown">
+                            <li><a href="#">Menu One</a></li>
+                            <li><a href="#">Menu Two</a></li>
+                            <li><a href="#">Menu Three</a></li>
+                          </ul>
+                        </li>
                       </ul>
                     </li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
                   </ul>
-                </li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-              </ul>
-            </nav>
+
+
+                  <? $APPLICATION->IncludeComponent(
+                    "bitrix:menu",
+                    ".default",
+                    array(
+                      "ALLOW_MULTI_SELECT" => "N",
+                      "CHILD_MENU_TYPE" => "left",
+                      "DELAY" => "N",
+                      "MAX_LEVEL" => "1",
+                      "MENU_CACHE_GET_VARS" => array(),
+                      "MENU_CACHE_TIME" => "3600",
+                      "MENU_CACHE_TYPE" => "Y",
+                      "MENU_CACHE_USE_GROUPS" => "Y",
+                      "ROOT_MENU_TYPE" => "top",
+                      "USE_EXT" => "N",
+                      "COMPONENT_TEMPLATE" => ".default"
+                    ),
+                    false
+                  ); ?>
+
+
+                </nav>
+              </div>
+
+
+            </div>
           </div>
-
-
         </div>
       </div>
-    </div>
-  </div>
